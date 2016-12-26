b8f466aac7dcbc6a5cb00ea098da730fb8f99aab
Matt Mullins <mmullins@mmlx.us>
2016-12-26 13:52:15 -0800

    Parses the server's response.
    This more or less matches xdpyinfo from what I can tell.

---
+++ src/bin/client_init.rs
@@ -0,0 +1,76 @@
+extern crate x11_client;
+use x11_client::*;
+
+use std::os::unix::net::UnixStream;
+use std::io::prelude::*;
+
+fn main() {
+    let mut socket = UnixStream::connect("/tmp/.X11-unix/X0").unwrap();
+
+    let client_init: Vec<_> = ClientInit::new().into();
+    socket.write(&client_init).unwrap();
+
+    let server_response = ServerInit::from_stream(&mut socket).unwrap();
+
+    println!("major: {}", server_response.major);
+    println!("minor: {}", server_response.minor);
+    println!("release_number: {}", server_response.release_number);
+    println!("resource_id_base: {}", server_response.resource_id_base);
+    println!("resource_id_mask: {}", server_response.resource_id_mask);
+    println!("motion_buffer_size: {}", server_response.motion_buffer_size);
+    println!("maximum_request_length: {}",
+        server_response.maximum_request_length);
+    println!("image_byte_order: {}", server_response.image_byte_order);
+    println!("bitmap_format_bit_order: {}",
+        server_response.bitmap_format_bit_order);
+    println!("bitmap_format_scanline_unit: {}",
+        server_response.bitmap_format_scanline_unit);
+    println!("bitmap_format_scanline_pad: {}",
+        server_response.bitmap_format_scanline_pad);
+    println!("min_keycode: {}", server_response.min_keycode);
+    println!("max_keycode: {}", server_response.max_keycode);
+    println!("vendor: {}", server_response.vendor);
+
+    println!("pixmap formats:");
+    for fmt in server_response.pixmap_formats {
+        println!("\tdepth: {}", fmt.depth);
+        println!("\tbits_per_pixel: {}", fmt.bits_per_pixel);
+        println!("\tscanline_pad: {}", fmt.scanline_pad);
+    }
+
+    println!("screens:");
+    for screen in server_response.roots {
+        println!("\troot: {}", screen.root);
+        println!("\tdefault_colormap: {}", screen.default_colormap);
+        println!("\twhite_pixel: {}", screen.white_pixel);
+        println!("\tblack_pixel: {}", screen.black_pixel);
+        println!("\tcurrent_input_masks: {}", screen.current_input_masks);
+        println!("\twidth_pixels: {}", screen.width_pixels);
+        println!("\theight_pixels: {}", screen.height_pixels);
+        println!("\twidth_millimeters: {}", screen.width_millimeters);
+        println!("\theight_millimeters: {}", screen.height_millimeters);
+        println!("\tmin_installed_maps: {}", screen.min_installed_maps);
+        println!("\tmax_installed_maps: {}", screen.max_installed_maps);
+        println!("\troot_visual: {}", screen.root_visual);
+        println!("\tbacking_stores: {}", screen.backing_stores);
+        println!("\tsave_unders: {}", screen.save_unders);
+        println!("\troot_depth: {}", screen.root_depth);
+
+        println!("\tdepths:");
+        for depth in screen.allowed_depths {
+            println!("\t\tdepth: {}", depth.depth);
+            println!("\t\tvisuals:");
+
+            for visual in depth.visuals {
+                println!("\t\t\tvisual_id: {}", visual.id);
+                println!("\t\t\tclass: {}", visual.class);
+                println!("\t\t\tbits_per_rgb_value: {}", visual.bits_per_rgb_value);
+                println!("\t\t\tcolormap_entries: {}", visual.colormap_entries);
+                println!("\t\t\tred_mask: {}", visual.red_mask);
+                println!("\t\t\tgreen_mask: {}", visual.green_mask);
+                println!("\t\t\tblue_mask: {}", visual.blue_mask);
+                println!("");
+            }
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,8 @@ mod tests;
 
 extern crate byteorder;
 
+use std::io::{Read, Write, Result};
+
 pub struct ClientInit<'a> {
     major: u16,
     minor: u16,
@@ -39,5 +41,234 @@ impl<'a> Into<Vec<u8>> for ClientInit<'a> {
         // the unused data needs to be sent, too.
         ret.write_u16::<BigEndian>(0);
         ret
+    }
+}
+
+pub struct ServerInit {
+    pub major: u16,
+    pub minor: u16,
+    pub release_number: u32,
+    pub resource_id_base: u32,
+    pub resource_id_mask: u32,
+    pub motion_buffer_size: u32,
+    pub maximum_request_length: u16,
+    pub image_byte_order: u8,
+    pub bitmap_format_bit_order: u8,
+    pub bitmap_format_scanline_unit: u8,
+    pub bitmap_format_scanline_pad: u8,
+    pub min_keycode: u8,
+    pub max_keycode: u8,
+    pub vendor: String,
+    pub pixmap_formats: Vec<PixmapFormat>,
+    pub roots: Vec<Screen>,
+}
+
+impl ServerInit {
+    pub fn from_stream<T: Read>(stream: &mut T) -> Result<ServerInit> {
+        use byteorder::{BigEndian, ReadBytesExt};
+
+        let success = stream.read_u8()?;
+        stream.read_u8()?;
+        let major = stream.read_u16::<BigEndian>()?;
+        let minor = stream.read_u16::<BigEndian>()?;
+        stream.read_u16::<BigEndian>()?;
+        let release_number = stream.read_u32::<BigEndian>()?;
+        let resource_id_base = stream.read_u32::<BigEndian>()?;
+        let resource_id_mask = stream.read_u32::<BigEndian>()?;
+        let motion_buffer_size = stream.read_u32::<BigEndian>()?;
+        let vendor_len = stream.read_u16::<BigEndian>()?;
+        let maximum_request_length = stream.read_u16::<BigEndian>()?;
+        let screen_count = stream.read_u8()?;
+        let pixmap_format_count = stream.read_u8()?;
+        let image_byte_order = stream.read_u8()?;
+        let bitmap_format_bit_order = stream.read_u8()?;
+        let bitmap_format_scanline_unit = stream.read_u8()?;
+        let bitmap_format_scanline_pad = stream.read_u8()?;
+        let min_keycode = stream.read_u8()?;
+        let max_keycode = stream.read_u8()?;
+        stream.read_u32::<BigEndian>()?;
+
+        let mut vendor_bytes = vec![0; vendor_len as usize];
+        stream.read_exact(&mut vendor_bytes)?;
+        let padding = (4 - (vendor_len % 4)) % 4;
+        for _ in 0..padding {
+            stream.read_u8()?;
+        }
+
+        let mut pixmap_formats = Vec::new();
+        for _ in 0..pixmap_format_count {
+            pixmap_formats.push(PixmapFormat::from_stream(stream)?);
+        }
+
+        let mut roots = Vec::new();
+        for _ in 0..screen_count {
+            roots.push(Screen::from_stream(stream)?);
+        }
+
+        Ok(ServerInit {
+            major: major,
+            minor: minor,
+            release_number: release_number,
+            resource_id_base: resource_id_base,
+            resource_id_mask: resource_id_mask,
+            motion_buffer_size: motion_buffer_size,
+            maximum_request_length: maximum_request_length,
+            image_byte_order: image_byte_order,
+            bitmap_format_bit_order: bitmap_format_bit_order,
+            bitmap_format_scanline_unit: bitmap_format_scanline_unit,
+            bitmap_format_scanline_pad: bitmap_format_scanline_pad,
+            min_keycode: min_keycode,
+            max_keycode: max_keycode,
+
+            pixmap_formats: pixmap_formats,
+            roots: roots,
+            vendor: String::from_utf8(vendor_bytes).unwrap(),
+        })
+    }
+}
+
+pub struct PixmapFormat {
+    pub depth: u8,
+    pub bits_per_pixel: u8,
+    pub scanline_pad: u8,
+}
+
+impl PixmapFormat {
+    pub fn from_stream<T: Read>(stream: &mut T) -> Result<Self> {
+        use byteorder::ReadBytesExt;
+        let depth = stream.read_u8()?;
+        let bits_per_pixel = stream.read_u8()?;
+        let scanline_pad = stream.read_u8()?;
+        stream.read_exact(&mut [0; 5])?;
+
+        Ok(PixmapFormat{
+            depth: depth,
+            bits_per_pixel: bits_per_pixel,
+            scanline_pad: scanline_pad,
+        })
+    }
+}
+
+pub struct Screen {
+    pub root: u32,
+    pub default_colormap: u32,
+    pub white_pixel: u32,
+    pub black_pixel: u32,
+    pub current_input_masks: u32,
+    pub width_pixels: u16,
+    pub height_pixels: u16,
+    pub width_millimeters: u16,
+    pub height_millimeters: u16,
+    pub min_installed_maps: u16,
+    pub max_installed_maps: u16,
+    pub root_visual: u32,
+    pub backing_stores: u8, // TODO: enum
+    pub save_unders: bool,
+    pub root_depth: u8,
+    pub allowed_depths: Vec<Depth>,
+}
+
+impl Screen {
+    pub fn from_stream<T: Read>(stream: &mut T) -> Result<Self> {
+        use byteorder::{BigEndian, ReadBytesExt};
+        let root = stream.read_u32::<BigEndian>()?;
+        let default_colormap = stream.read_u32::<BigEndian>()?;
+        let white_pixel = stream.read_u32::<BigEndian>()?;
+        let black_pixel = stream.read_u32::<BigEndian>()?;
+        let current_input_masks = stream.read_u32::<BigEndian>()?;
+        let width_pixels = stream.read_u16::<BigEndian>()?;
+        let height_pixels = stream.read_u16::<BigEndian>()?;
+        let width_millimeters = stream.read_u16::<BigEndian>()?;
+        let height_millimeters = stream.read_u16::<BigEndian>()?;
+        let min_installed_maps = stream.read_u16::<BigEndian>()?;
+        let max_installed_maps = stream.read_u16::<BigEndian>()?;
+        let root_visual = stream.read_u32::<BigEndian>()?;
+        let backing_stores = stream.read_u8()?;
+        let save_unders = stream.read_u8()? != 0;
+        let root_depth = stream.read_u8()?;
+        let depth_count = stream.read_u8()?;
+
+        let mut allowed_depths = Vec::new();
+        for _ in 0..depth_count {
+            allowed_depths.push(Depth::from_stream(stream)?);
+        }
+
+        Ok(Screen {
+            root: root,
+            default_colormap: default_colormap,
+            white_pixel: white_pixel,
+            black_pixel: black_pixel,
+            current_input_masks: current_input_masks,
+            width_pixels: width_pixels,
+            height_pixels: height_pixels,
+            width_millimeters: width_millimeters,
+            height_millimeters: height_millimeters,
+            min_installed_maps: min_installed_maps,
+            max_installed_maps: max_installed_maps,
+            root_visual: root_visual,
+            backing_stores: backing_stores,
+            save_unders: save_unders,
+            root_depth: root_depth,
+            allowed_depths: allowed_depths,
+        })
+    }
+}
+
+pub struct Depth {
+    pub depth: u8,
+    pub visuals: Vec<Visual>,
+}
+
+impl Depth {
+    pub fn from_stream<T: Read>(stream: &mut T) -> Result<Self> {
+        use byteorder::{BigEndian, ReadBytesExt};
+        let depth = stream.read_u8()?;
+        stream.read_u8()?;
+        let visual_count = stream.read_u16::<BigEndian>()?;
+        stream.read_u32::<BigEndian>()?;
+
+        let mut visuals = Vec::new();
+        for _ in 0..visual_count {
+            visuals.push(Visual::from_stream(stream)?);
+        }
+
+        Ok(Depth {
+            depth: depth,
+            visuals: visuals,
+        })
+    }
+}
+
+pub struct Visual {
+    pub id: u32,
+    pub class: u8, // TODO: enum
+    pub bits_per_rgb_value: u8,
+    pub colormap_entries: u16,
+    pub red_mask: u32,
+    pub green_mask: u32,
+    pub blue_mask: u32,
+}
+
+impl Visual {
+    pub fn from_stream<T: Read>(stream: &mut T) -> Result<Self> {
+        use byteorder::{BigEndian, ReadBytesExt};
+        let id = stream.read_u32::<BigEndian>()?;
+        let class = stream.read_u8()?;
+        let bits_per_rgb_value = stream.read_u8()?;
+        let colormap_entries = stream.read_u16::<BigEndian>()?;
+        let red_mask = stream.read_u32::<BigEndian>()?;
+        let green_mask = stream.read_u32::<BigEndian>()?;
+        let blue_mask = stream.read_u32::<BigEndian>()?;
+        stream.read_u32::<BigEndian>()?;
+
+        Ok(Visual {
+            id: id,
+            class: class,
+            bits_per_rgb_value: bits_per_rgb_value,
+            colormap_entries: colormap_entries,
+            red_mask: red_mask,
+            green_mask: green_mask,
+            blue_mask: blue_mask,
+        })
     }
 }
